093c76faabc4cc20f537e36e5611ed64dd282535
Rumi Ridwan <ridwan@Rumis-MacBook-Pro.local>
2025-06-28 18:19:38 +0700

    send audio

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 import io from "socket.io-client";
 import * as fs from "fs";
+import "dotenv/config";
 
 const audio = [
   {
-    format: "ulaw",
-    total: 5,
+    format: "alaw",
+    total: 9,
+    extension: "alaw",
+    chunks: [],
+  },
+  {
+    format: "mp3",
+    total: 10,
+    extension: "mp3",
+    chunks: [],
+  },
+  {
+    format: "mulaw",
+    total: 9,
+    extension: "mulaw",
+    chunks: [],
+  },
+  {
+    format: "pcm16",
+    total: 9,
+    extension: "pcm",
+    chunks: [],
   },
 ];
+
+const buildAudio = () => {
+  return audio.map((item) => ({
+    ...item,
+    chunks: Array.from({ length: 9 }, (_, i) => i).map((chunk) =>
+      fs.readFileSync(
+        `./audio/${item.format}/output_${String(chunk).padStart(3, "0")}.${
+          item.extension
+        }`
+      )
+    ),
+  }));
+};
+
+const audioData = buildAudio();
+
+const sendAudio = async (callback) => {
+  const format = audioData.shift();
+  if (format) {
+    const sending = (callback) => {
+      const audio = format.chunks.shift();
+      if (audio) {
+        setTimeout(() => {
+          callback(
+            format.format,
+            `label ${format} ${format.total - format.chunks.length}`,
+            audio
+          );
+          sending(callback);
+        }, 200);
+      } else {
+        sendAudio(callback);
+      }
+    };
+    sending(callback);
+  }
+};
+
+//process.exit();
 
 const socket = io(process.env.SOCKET_SERVER, {
   reconnectionDelayMax: 10000,
@@ -52,6 +112,16 @@ socket.on("hangup", (data) => {
 
 socket.on("dialStatus", (status) => {
   console.log("dial status", status);
+  if (status.status === "Connected") {
+    sendAudio((format, label, chunk) => {
+      socket.emit("audio", {
+        sessionId: status.sessionId,
+        audioData: chunk,
+        audioFormat: format,
+        label: label,
+      });
+    });
+  }
 });
 
 socket.on("connect_error", (err) => {
