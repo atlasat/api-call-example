209243c71cc358b8acadd71cbb927e9c08ef6445
Rumi Ridwan <ridwan@Rumis-MacBook-Pro.local>
2025-06-29 13:58:50 +0700

    play per session

--- index.js
+++ index.js
@@ -1,72 +1,81 @@
 import io from "socket.io-client";
 import * as fs from "fs";
 import "dotenv/config";
+import { resolve } from "path";
 
 const audio = [
   {
     format: "alaw",
     total: 9,
     extension: "alaw",
+    duration: 25000,
     chunks: [],
   },
   {
     format: "mp3",
-    total: 10,
+    total: 9, //10
     extension: "mp3",
+    duration: 27000,
     chunks: [],
   },
   {
     format: "mulaw",
     total: 9,
     extension: "mulaw",
+    duration: 25000,
     chunks: [],
   },
   {
     format: "pcm16",
     total: 9,
     extension: "pcm",
+    duration: 25000,
     chunks: [],
   },
 ];
 
 const buildAudio = () => {
-  return audio.map((item) => ({
-    ...item,
-    chunks: Array.from({ length: 9 }, (_, i) => i).map((chunk) =>
-      fs.readFileSync(
-        `./audio/${item.format}/output_${String(chunk).padStart(3, "0")}.${
-          item.extension
-        }`
-      )
-    ),
-  }));
+  return [
+    ...audio.map((item) => ({
+      ...item,
+      chunks: Array.from({ length: item.total }, (_, i) => i).map((chunk) =>
+        fs.readFileSync(
+          `./audio/${item.format}/output_${String(chunk).padStart(3, "0")}.${
+            item.extension
+          }`
+        )
+      ),
+    })),
+  ];
 };
 
-const audioData = buildAudio();
-
-const sendAudio = async (callback) => {
-  const format = audioData.shift();
-  if (format) {
-    const sending = (callback) => {
-      const audio = format.chunks.shift();
-      if (audio) {
-        setTimeout(() => {
-          callback(
-            format.format,
-            `label ${format} ${format.total - format.chunks.length}`,
-            audio
-          );
-          sending(callback);
-        }, 200);
-      } else {
-        sendAudio(callback);
+//const getAllAudio = buildAudio();
+const playAudio = (audio, callback) => {
+  if (audio.length > 0) {
+    const format = audio[0];
+    if (format) {
+      const chunk = format.chunks.shift();
+      if (format.chunks.length === 0) {
+        audio.shift();
       }
-    };
-    sending(callback);
+      if (chunk) {
+        callback(
+          format.format,
+          `label ${format.format} ${format.total - format.chunks.length}`,
+          chunk,
+          () => {
+            if (format.chunks.length === 0) {
+              audio.shift();
+            }
+            playAudio(callback);
+          }
+        );
+      } else {
+        audio.shift();
+      }
+    }
   }
 };
-
-//process.exit();
 
 const socket = io(process.env.SOCKET_SERVER, {
   reconnectionDelayMax: 10000,
@@ -81,7 +90,9 @@ socket.on("connect", () => {
 });
 
 let sessionId;
+let allAudioSession = {};
 socket.on("newSession", (data) => {
+  allAudioSession[data.sessionId] = buildAudio();
   console.log("newSession", data);
   socket.emit("joinRoom", { sessionId: data.sessionId });
   sessionId = data.sessionId;
@@ -100,6 +111,20 @@ socket.on("audioCompleted", (data) => {
 
 socket.on("audioPlaying", (data) => {
   console.log("audio playing: ", data);
+  playAudio(allAudioSession[data.sessionId], (format, label, audioBuffer) => {
+    socket.emit(
+      "audio",
+      {
+        sessionId: data.sessionId,
+        audioData: audioBuffer,
+        audioFormat: format,
+        label: label,
+      },
+      (ack) => {
+        console.log("ack", ack);
+      }
+    );
+  });
 });
 
 socket.on("roomJoined", (data) => {
@@ -110,22 +135,89 @@ socket.on("hangup", (data) => {
   console.log("hangup call", data);
 });
 
-socket.on("dialStatus", (status) => {
+socket.on("dialStatus", async (status) => {
   console.log("dial status", status);
   if (status.status === "Connected") {
-    sendAudio((format, label, chunk) => {
-      socket.emit("audio", {
-        sessionId: status.sessionId,
-        audioData: chunk,
-        audioFormat: format,
-        label: label,
-      });
-    });
+    playAudio(
+      allAudioSession[status.sessionId],
+      (format, label, audioBuffer) => {
+        socket.emit(
+          "audio",
+          {
+            sessionId: status.sessionId,
+            audioData: audioBuffer,
+            audioFormat: format,
+            label: label,
+          },
+          (ack) => {
+            console.log("ack", ack);
+          }
+        );
+      }
+    );
+    // socket.emit("audio", {
+    //   sessionId: status.sessionId,
+    //   audioData: fs.readFileSync("./audio/tomy_G711.org_.wav"),
+    //   label: "tomy page",
+    //   audioFormat: "mulaw",
+    // });
+    // const readStream = fs.createReadStream("./audio/tomy_G711.org_.wav", {
+    //   highWaterMark: 1024 * 10, // 2KB
+    // });
+    // const chunks = [];
+    // readStream.on("data", async (chunk) => {
+    //   console.log(`Received ${chunk.length} bytes`);
+    //   // Proses chunk di sini
+    //   chunks.push(
+    //     new Promise((resolve) => {
+    //       socket.emit("audio", {
+    //         sessionId: status.sessionId,
+    //         audioData: chunk,
+    //         label: "tomy page" + Math.random(),
+    //         audioFormat: "mulaw",
+    //       });
+    //       setTimeout(resolve, 1260);
+    //     })
+    //   );
+    // });
+    // await Promise.race(chunks);
+    // await new Promise((resolve) => {
+    //   readStream.on("end", () => {
+    //     console.log("Done reading file.");
+    //     resolve(true);
+    //   });
+    // });
+    //socket.emit("interruption", { sessionId: status.sessionId });
+    // const controller = new AudioController(audio);
+    // controller
+    //   .playAll((format, label, audioBuffer) => {
+    //     console.log(`Playing: ${label}`);
+    //     // Process audio buffer here
+    //     socket.emit(
+    //       "audio",
+    //       {
+    //         sessionId: status.sessionId,
+    //         audioData: audioBuffer,
+    //         audioFormat: format,
+    //         label: label,
+    //       },
+    //       (ack) => {
+    //         console.log("ack", ack);
+    //       }
+    //     );
+    //   })
+    //   .then(() => {
+    //     console.log("Controller: All completed and cleared!");
+    //   });
   }
 });
 
 socket.on("connect_error", (err) => {
   console.log(err); // prints the message associated with the error
+});
+
+socket.on("error", (data) => {
+  console.log("error: ", data);
 });
 
 socket.on("audio", (data) => {});
